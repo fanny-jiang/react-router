7865fc66178fcfe0d25221038721710cd516b2f0
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-03-28 16:59:08 -0400

    Finished Our "Picture Frame"

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,11 @@
+import { Router, Route, hashHistory } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
 
-ReactDOM.render(
-  <AppContainer />,
-  document.getElementById('app')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+    </Route>
+  </Router>
+), document.getElementById('app'));
