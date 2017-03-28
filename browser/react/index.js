436b33764b69183c76db2c61db6c172c33251c15
Fanny Jiang <jiangf@bu.edu>
2017-03-28 17:38:02 -0400

    redirected albums route to /

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,11 +1,17 @@
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import Albums from '../react/components/Albums';
+import Album from '../react/components/Album';
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={AppContainer}>
+      <Route path="/albums" component={Albums} />
+      <IndexRedirect to="/albums" />
     </Route>
   </Router>
 ), document.getElementById('app'));
+
+
