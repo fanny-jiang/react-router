10a1bdfdaf161ad416060cdbb273e6c9e23d2885
Fanny Jiang <jiangf@bu.edu>
2017-03-29 15:37:49 -0400

    completed bonus active class links

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -9,13 +9,13 @@ const Sidebar = (props) => {
     <sidebar>
       <img src="/juke.svg" className="logo" />
       <section>
-        <h4 className="menu-item active">
-        <Link to="/albums/">ALBUMS</Link>
+        <h4>
+        <Link to="/albums/" className="menu-item" activeClassName="active">ALBUMS</Link>
         </h4>
       </section>
       <section>
-        <h4 className="menu-item">
-          <Link to="/artists/">ARTISTS</Link>
+        <h4>
+          <Link to="/artists/" className="menu-item" activeClassName="active">ARTISTS</Link>
         </h4>
       </section>
     </sidebar>
